dbb7c9aa1ecf188fc587f6526539a5626be9b298
Joonas Nordlund <jonaz92@gmail.com>
2020-11-24 18:55:52 +0200

    Intitialize Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import octocat from "./img/octocat.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Jokes on Chuck!</h1>
+        <p>A React app for displaying Chuck Norris jokes from [The Internet Chuck Norris Database](http://www.icndb.com).</p>
+
       </header>
+      <footer className="app-footer">
+        <a href="https://github.com/snaguf/jokesonchuck">
+          <img className="logo" src={octocat} alt="GitHub" />
+        </a>
+        <a className="license" href="https://github.com/snaguf/jokesonchuck/blob/main/LICENSE.md">&copy; 2020 Joonas Nordlund</a>
+      </footer>
     </div>
   );
 }
