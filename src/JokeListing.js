24d4f3077059694d5ad536ea37761005c28900b5
Joonas Nordlund <jonaz92@gmail.com>
2020-11-26 21:23:50 +0200

    Add component for no joke indicator

--- src/JokeListing.js
+++ src/JokeListing.js
@@ -1,5 +1,7 @@
 import "./JokeListing.css"
 
+import walker from "./img/walker.png"
+
 const Joke = ({ text, direction }) => {
   return <section className={`message -${direction}`}>
     <div className={`nes-balloon from-${direction} is-dark`}>
@@ -8,6 +10,16 @@ const Joke = ({ text, direction }) => {
   </section>
 }
 
+const NoJokes = () => {
+  return <div className="walker-joke -right">
+    <section className="messge walker-message">
+      <div className="nes-balloon from-right is-dark ">
+        <p>There are no jokes!</p>
+      </div>
+    </section>
+    <img className="walker -right" src={walker} alt="Brodel Walker" />
+  </div>
+}
 
 const JokeListing = ({ jokes }) => {
   return <div className="joke-listing nes-container is-rounded is-dark">
