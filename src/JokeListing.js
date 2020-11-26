197114ac2994dfdcb75f297219fee0e0a52a23f4
Joonas Nordlund <jonaz92@gmail.com>
2020-11-26 23:12:47 +0200

    Show indicator when there are no jokes
    * Add component for no joke indicator
    
    * Show indicator when jokes list is empty
    
    * Show fetch error message if there is one

--- src/JokeListing.js
+++ src/JokeListing.js
@@ -1,5 +1,7 @@
 import "./JokeListing.css"
 
+import walker from "./img/walker.png"
+
 const Joke = ({ text, direction }) => {
   return <section className={`message -${direction}`}>
     <div className={`nes-balloon from-${direction} is-dark`}>
@@ -8,13 +10,30 @@ const Joke = ({ text, direction }) => {
   </section>
 }
 
+const NoJokes = ({ message }) => {
+  return <div className="walker-joke -right">
+    <section className="messge walker-message">
+      <div className="nes-balloon from-right is-dark ">
+        <p>{message}</p>
+      </div>
+    </section>
+    <img className="walker -right" src={walker} alt="Brodel Walker" />
+  </div>
+}
 
-const JokeListing = ({ jokes }) => {
+const Jokes = ({ jokes }) => {
+  return jokes.map(({ joke }, i) => {
+    return <Joke text={joke} direction={i % 2 === 0 ? "left" : "right"} />
+  })
+}
+
+const JokeListing = ({ jokes, error }) => {
+
+  const message = error ? error.message : "There are no jokes! Fetch!"
+
   return <div className="joke-listing nes-container is-rounded is-dark">
     <div className="message-list">
-      {jokes.map(({ joke }, i) => {
-        return <Joke text={joke} direction={i % 2 === 0 ? "left" : "right"} />
-      })}
+      {jokes.length === 0 ? <NoJokes message={message} /> : <Jokes jokes={jokes} />}
     </div>
   </div>
 }
