86a201e8979df7adde55730e4f18d16b4aaff339
Joonas Nordlund <jonaz92@gmail.com>
2020-11-27 00:04:27 +0200

    Add controllable number input for joke amount

--- src/JokeSearch.js
+++ src/JokeSearch.js
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import "./JokeSearch.css"
 
 const JokeSearch = ({ onFetch: fetchJokes, loading }) => {
+  const [jokeAmount, setJokeAmount] = useState(10);
 
-  return <div className="joke-search">
-    <button
-      className="nes-btn is-primary"
-      onClick={e => { if (!loading) fetchJokes("https://api.icndb.com/jokes/random/10?escape=javascript") }}
-    >
-      Fetch Jokes
+  const handleAmountChange = (input) => {
+    const amount = Number(input)
+    if (amount && amount >= 1) setJokeAmount(amount)
+  }
+
+  return (
+    <div className="joke-search nes-container is-rounded is-dark">
+      <div className="nes-field is-inline">
+        <label for="joke-amount">Joke Count</label>
+        <input type="number" id="joke-amount" className="nes-input" value={jokeAmount} onChange={e => { handleAmountChange(e.target.value) }} />
+      </div>
+      <button
+        className="nes-btn is-primary"
+        onClick={e => { if (!loading) fetchJokes("https://api.icndb.com/jokes/random/10?escape=javascript") }}
+      >
+        Fetch Jokes
     </button>
-  </div>
+    </div>
+  )
 }
 
 export default JokeSearch;
