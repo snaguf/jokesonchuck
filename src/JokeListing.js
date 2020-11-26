4c9ba72e5c8e18d2605e3db8bfac28100b47a6b0
Joonas Nordlund <jonaz92@gmail.com>
2020-11-26 15:48:53 +0200

    Add component for single joke

--- src/JokeListing.js
+++ src/JokeListing.js
@@ -1,9 +1,20 @@
 import "./JokeListing.css"
 
+const Joke = ({ text, direction }) => {
+  return <section className={`-${direction}`}>
+    <div className={`nes-balloon from-${direction} is-dark`}>
+      <p>{text}</p>
+    </div>
+  </section>
+}
+
 
 const JokeListing = () => {
   return <div className="joke-listing nes-container is-rounded is-dark">
-    <p>Jokes here!</p>
+    <div className="message-list">
+      <Joke text="Joke here!" direction="left" />
+      <Joke text="Joke also here!" direction="right" />
+    </div>
   </div>
 }
 
