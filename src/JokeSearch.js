4e86ed080331f62e2e2f2833944ee55a9f707661
Joonas Nordlund <jonaz92@gmail.com>
2020-11-26 17:11:30 +0200

    Fix fetch problems
    * Fix #11 Joke encoding
    
    Add query parameter escape=javascript so special characters are not escaped with HTML encoding.
    
    * Fix #13 Fetching jokes on deployed app
    
    Change API url to use HTTPS instead of HTTP so there should not be problems with mixed active content.

--- src/JokeSearch.js
+++ src/JokeSearch.js
@@ -5,7 +5,7 @@ const JokeSearch = ({ onFetch: fetchJokes, loading }) => {
   return <div className="joke-search">
     <button
       className="nes-btn is-primary"
-      onClick={e => { if (!loading) fetchJokes("http://api.icndb.com/jokes/random/10") }}
+      onClick={e => { if (!loading) fetchJokes("https://api.icndb.com/jokes/random/10?escape=javascript") }}
     >
       Fetch Jokes
     </button>
