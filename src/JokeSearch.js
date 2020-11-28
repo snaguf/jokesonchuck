70204386d43c443646b3f842bd21aba5ecfa89ea
Joonas Nordlund <jonaz92@gmail.com>
2020-11-28 19:25:13 +0200

    Add input validity checks
    It is now possible to clear the input field, there is an error indicator that value is missing.

--- src/JokeSearch.js
+++ src/JokeSearch.js
@@ -4,16 +4,18 @@ import "./JokeSearch.css"
 const JokeSearch = ({ onFetch: fetchJokes, loading }) => {
   const [jokeAmount, setJokeAmount] = useState(10);
 
-  const handleAmountChange = (input) => {
-    const amount = Number(input)
-    if (amount && amount >= 1) setJokeAmount(amount)
+  const handleAmountChange = (e) => {
+    if (e.target.validity.valueMissing) setJokeAmount(null)
+    if (e.target.checkValidity()) setJokeAmount(Number(e.target.value))
   }
 
   return (
     <div className="joke-search nes-container is-rounded is-dark">
       <div className="nes-field is-inline">
         <label for="joke-amount">Joke Count</label>
-        <input type="number" id="joke-amount" className="nes-input" value={jokeAmount} onChange={e => { handleAmountChange(e.target.value) }} />
+        <input type="number" min={1} required={true} id="joke-amount"
+          className={`nes-input ${!jokeAmount ? "is-error" : ""}`} value={jokeAmount}
+          onChange={e => { handleAmountChange(e) }} />
       </div>
       <button
         className="nes-btn is-primary"
