d3e6ee8b409d8ca5e8a686a1c5b1f7af87dda471
Joonas Nordlund <jonaz92@gmail.com>
2020-11-30 08:25:17 +0200

    Joke amount (#16)
    * Add controllable number input for joke amount
    
    * Add input validity checks
    
    It is now possible to clear the input field, there is an error indicator that value is missing.
    
    * Disable fetch button when joke amount is not set
    
    * Fetch jokes with set amount

--- src/JokeSearch.js
+++ src/JokeSearch.js
@@ -1,15 +1,33 @@
+import { useState } from "react"
 import "./JokeSearch.css"
 
 const JokeSearch = ({ onFetch: fetchJokes, loading }) => {
+  const [jokeAmount, setJokeAmount] = useState(10);
 
-  return <div className="joke-search">
-    <button
-      className="nes-btn is-primary"
-      onClick={e => { if (!loading) fetchJokes("https://api.icndb.com/jokes/random/10?escape=javascript") }}
-    >
-      Fetch Jokes
+  const handleAmountChange = (e) => {
+    if (e.target.validity.valueMissing) setJokeAmount(null)
+    if (e.target.checkValidity()) setJokeAmount(Number(e.target.value))
+  }
+
+  const jokeUrl = `https://api.icndb.com/jokes/random/${jokeAmount}?escape=javascript`
+
+  return (
+    <div className="joke-search nes-container is-rounded is-dark">
+      <div className="nes-field is-inline">
+        <label for="joke-amount">Joke Count</label>
+        <input type="number" min={1} required={true} id="joke-amount"
+          className={`nes-input ${!jokeAmount ? "is-error" : ""}`} value={jokeAmount}
+          onChange={e => { handleAmountChange(e) }} />
+      </div>
+      <button
+        className={`nes-btn ${jokeAmount ? "is-primary" : "is-disabled"}`}
+        disabled={!jokeAmount}
+        onClick={e => { if (!loading) fetchJokes(jokeUrl) }}
+      >
+        Fetch Jokes
     </button>
-  </div>
+    </div>
+  )
 }
 
 export default JokeSearch;
