ead8d9d1397ba903e4c44fd13610cfbc786a6d3b
Joonas Nordlund <jonaz92@gmail.com>
2020-11-30 18:08:32 +0200

    Add test for joke count to be set less than 1

--- src/JokeSearch.test.js
+++ src/JokeSearch.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import JokeSearch from './JokeSearch';
 
+const setup = () => {
+  const rend = render(<JokeSearch categories={[]} />);
+  const input = rend.getByLabelText("Joke Count");
+  return input;
+}
+
 test('Default count should be 10', () => {
-  render(<JokeSearch categories={[]} />);
-  const elem = screen.getByLabelText("Joke Count");
+  const elem = setup();
   const val = Number(elem.value)
   expect(val).toBe(10)
 });
+
+test("Count can't be set less than 1", () => {
+  const elem = setup();
+  fireEvent.change(elem, { target: { value: -1 } });
+  let val = Number(elem.value)
+  expect(val).not.toBe(-1);
+  expect(val).toBe(10);
+
+  fireEvent.change(elem, { target: { value: 5 } })
+  val = Number(elem.value);
+  expect(val).toBe(5);
+})
